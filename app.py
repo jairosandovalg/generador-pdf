b57f2eff9b433045af499ecde889df7c724c9ec8
JairoSG <jairosandovalg011@gmail.com>
2026-06-25 08:40:00 -0500

    app.py

--- app.py
+++ app.py
@@ -22,9 +22,13 @@ SUPABASE_URL = os.environ.get("SUPABASE_URL", "TU_SUPABASE_URL_AQUI")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "TU_SUPABASE_ANON_KEY_AQUI")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-#@app.route conecta la web con el código python (Routing)
+"""
+@app.route conecta la web con el código python (Routing)
+"""
 
-@app.route('/')
+@app.route('/') #Define que esta función responderá cuando se acceda a la raíz del sitio web.                                    
+
+
 def home():
     fecha_hoy = datetime.now().strftime('%Y-%m-%d')
     # Enviamos marca='audi' por defecto para la primera carga web
